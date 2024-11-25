9dd9f58793f7ecabdd67ca693b37ad5423033471
unknown <anikathakur212@gmail.com>
2024-11-25 17:35:06 -0500

    implemented method functionality

--- Hey/src/Main.java
+++ Hey/src/Main.java
@@ -1,17 +1,20 @@
 @FunctionalInterface
-
 interface Square {
-    int calculate(int x);
+    int calculate(int x, int y);
 }
 
 class Main {
-    public static void main(String args[])
-    {
-        int a = 5;
+    public static void main(String args[]) {
 
-        Square s = (int x) -> x + x;
+        Square s = ((int x, int y) -> {
+            int a = 0;
+            for (int i = x; i <= y; i++) {
+                a += i;  
+            }
+            return a;  
+        });
 
-        int ans = s.calculate(a);
-        System.out.println(ans);
+        int ans = s.calculate(5, 25);  
+        System.out.println(ans);  
     }
 }
